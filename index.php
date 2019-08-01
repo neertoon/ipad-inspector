affbc199980198473917e6944b5bdc0d5f1c4ab8
Mateusz Brzozowski <neertoon@icloud.com>
2019-08-01 10:08:56 +0200

    Strona startowa

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>The HTML5 Herald</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+</head>
+
+<body>
+    <p>
+        To tylko test
+    </p>
+</body>
+</html>
